896525c1364240223d110397f32f0104ed13fb2e
suhyeod <133014897+suhyeod@users.noreply.github.com>
2023-09-25 14:48:13 +0900

    Update js.js

---
+++ js/js.js
@@ -0,0 +1,19 @@
+function img_() {
+    $('#img').show();
+    $('#about').hide();
+    $('#pf').hide();
+}
+
+
+function about_() {
+    $('#about').show();
+    $('#img').hide();
+    $('#pf').hide();
+}
+
+
+function pf_() {
+    $('#pf').show();
+    $('#img').hide();
+    $('#about').hide();
+}
